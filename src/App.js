c99488b9f381fa0f6af8ff72713227c9514c11c1
nshaughn <nathanpaulshaughnessy@hotmail.co.uk>
2022-09-21 20:36:02 +0100

    shows the fruit when logged in

--- src/App.js
+++ src/App.js
@@ -1,61 +1,45 @@
 import { useState, useEffect } from "react";
-import "./App.css"
-import { faker } from '@faker-js/faker';
+// import "./App.css"
+// import { faker } from '@faker-js/faker';
 import UserModal from "./components/UserModal";
 import AccountModal from "./components/AccountModal";
+import FruitCard from "./components/FruitCard";
+// import SearchIcon from "./search.svg"
+
 
 
 const App = () => {
-  const [user, setUser] = useState()
+
   const [errorMsg, setErrorMsg] = useState('');
+  const [fruits, setFruits] = useState([])
+  const [user, setUser] = useState()
   const [token, setToken] = useState("")
   const [toggle, setToggle] = useState(false);
 
-  useEffect (() =>
-  {
-    const fetchFruit = async () =>
-    {
-      try {
-        setErrorMsg('');
-        const response = await fetch('http://localhost:9001/getFruity', {
-          method: 'GET',
-          headers: {"Content-Type": "application/json"}
-        })
-        
-        const data = await response.json();
-        console.log(data)
-      
-        const fruitDetails = data.map(() =>
-        {
-
-          return {
-            id: fruitEntry.id,
-            fruitIndex: index,
-            name: fruitEntry.name,
-            genus: fruitEntry.genus,
-            family: fruitEntry.family,
-            order: fruitEntry.order,
-            nutritions: fruitEntry.nutritions,
-            fruitPrice: Math.ceil(faker.datatype.number() / 2)
-
-          }
-        })
-        
-        
-        console.log(fruitDetails)
-
-
-      } catch (error) {
-        setErrorMsg('Oops, something went wrong')
-      }
-    };
-    fetchFruit();
-  }, [])
-
-  if (errorMsg !== '') {
-    return <h1>{errorMsg}</h1>
-  }
-
+  useEffect (() => {
+    fetchFruit()
+  },[])
+  
+      const fetchFruit = async () => {
+        try {
+          setErrorMsg('');
+          const response = await fetch('http://localhost:9001/getFruity', {
+            method: 'GET',
+            headers: {"Content-Type": "application/json"}
+          })
+          
+          const data = await response.json();
+          console.log(data)
+          setFruits(data)
+        } catch (error) {
+          setErrorMsg('Oops, something went wrong')
+        }
+      };
+  
+    if (errorMsg !== '') {
+      return <h1>{errorMsg}</h1>
+    }
+  
   return (
     <div className="main">
       <div className="contrast">
@@ -76,6 +60,19 @@ const App = () => {
             <br></br>
             <br></br>
             <h2>User: {user} is logged in</h2>
+
+            {fruits?.length > 0 ? (
+              <div>
+                {fruits.map((fruit) => (
+                  <FruitCard fruit={fruit} />
+                ))}
+              </div>
+            ) : (
+              <div>
+                <h1>NO FRUIT!!!!!!!!</h1>
+              </div>
+            )}
+
             <div className="navDiv">
               <div id="manageNav">
                 <AccountModal token={token} />
