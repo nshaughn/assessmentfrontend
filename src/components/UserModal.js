c70eb6de0d71efda724ad34936992001eab03cb4
Brett Garrett <b.t.garrett@aol.co.uk>
2022-09-22 16:57:28 +0100

    made recent pushes compatable

--- src/components/UserModal.js
+++ src/components/UserModal.js
@@ -6,37 +6,33 @@ import Login from './Login';
 // import DisplayMovies from "./DisplayMovies";
 Modal.setAppElement('#root');
 
-const UserModal = ({ setter, setToken, movieListState, setMovieListState, toggle, setToggle }) => {
-    const [loginStateModal, showLoginStateModal] = useState(false)
-    const [registerStateModal, showRegisterStateModal] = useState(false)
-    function openLoginModal() { showLoginStateModal(true); }
-    function openRegisterModal() { showRegisterStateModal(true); }
-    function closeLoginModal() { showLoginStateModal(false); }
-    function closeRegisterModal() { showRegisterStateModal(false); }
+const UserModal = ({ setter, setToken, closeLoginModal, closeRegisterModal, loginStateModal, registerStateModal }) => {
+    // const [loginStateModal, showLoginStateModal] = useState(false)
+    // const [registerStateModal, showRegisterStateModal] = useState(false)
+    // function closeLoginModal() { showLoginStateModal(false); }
+    // function closeRegisterModal() { showRegisterStateModal(false); }
 
     return (
         <div>
-            <div className="login">
-                <div>
-                   <div><a onClick={openLoginModal}  href="">Login</a> <a onClick={openRegisterModal}  href="">Register</a> </div>
-                    <Modal isOpen={loginStateModal} onRequestClose={closeLoginModal} className="modalContent" contentLabel="Example Modal" overlayClassName="modalZ">
-                        <Login setter={setter} setToken={setToken} />
-                    </Modal>
-                </div>
-                {/* <div>
+            <div>
+                {/* <div><a onClick={openLoginModal}>Login</a> <a onClick={openRegisterModal}>Register</a> </div> */}
+                <Modal isOpen={loginStateModal} onRequestClose={closeLoginModal} className="modalContent" contentLabel="Example Modal" overlayClassName="modalZ">
+                    <Login setter={setter} setToken={setToken} />
+                </Modal>
+            </div>
+            {/* <div>
                     <DisplayMovies movieListState={movieListState} setMovieListState={setMovieListState} toggle={toggle} setToggle={setToggle} />
                 </div> */}
-                <div>
+            <div>
                 <div></div>
-                    <Modal isOpen={registerStateModal} onRequestClose={closeRegisterModal} className="modalContent" contentLabel="Example Modal" overlayClassName="modalZ">
-                        <Register />
-                    </Modal>
-                </div>
-                    {/* {movieListState.map((movies, index) => (<div><h3 key={index}>Title: &nbsp;&nbsp;&nbsp;{movies.title}<br></br>Lead: &nbsp;&nbsp;&nbsp;{movies.actor}</h3></div>))} */}
+                <Modal isOpen={registerStateModal} onRequestClose={closeRegisterModal} className="modalContent" contentLabel="Example Modal" overlayClassName="modalZ">
+                    <Register />
+                </Modal>
             </div>
+            {/* {movieListState.map((movies, index) => (<div><h3 key={index}>Title: &nbsp;&nbsp;&nbsp;{movies.title}<br></br>Lead: &nbsp;&nbsp;&nbsp;{movies.actor}</h3></div>))} */}
             {/* <div className="movieList"> */}
-                {/* <div className={toggle ? "showUser" : "hideUser"}> */}
-                {/* </div> */}
+            {/* <div className={toggle ? "showUser" : "hideUser"}> */}
+            {/* </div> */}
             {/* </div> */}
         </div>
     )
